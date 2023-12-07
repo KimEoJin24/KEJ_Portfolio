523c5a00385c9d3d45e015b65a602e09c4e0545a
KimEoJin24 <kamejk24@gmail.com>
2023-12-08 08:56:31 +0900

    [ADD] StageClear

--- KEJ_Portfolio/Assets/TSEROF/Scripts/DataManagement/Data/StageClear.cs
+++ KEJ_Portfolio/Assets/TSEROF/Scripts/DataManagement/Data/StageClear.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageClear : MonoBehaviour
+public class StageClear : MonoBehaviour, IDataPersistence
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] stagePortal;
+    private bool[] _clear;
+
+    public void LoadData(GameData data)
     {
-        
+        for(int i=0; i<data.stageClear.Length; i++)
+        {
+            this._clear[i] = data.stageClear[i];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveData(GameData data)
     {
-        
+        for (int i = 0; i < data.stageClear.Length; i++)
+        {
+            this._clear[i] = data.stageClear[i];
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            for(int i=0; i< stagePortal.Length; i++) 
+            {
+                if (stagePortal[i])
+                {
+                    _clear[i] = true;
+                }
+            }
+        }
     }
 }
